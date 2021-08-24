4aac7f29e24b6058f715e4b580d762732acb7198
tinghan33704 <tinghan33704@gmail.com>
2021-08-24 21:37:02 +0800

    add css

--- js/common.js
+++ js/common.js
@@ -737,11 +737,14 @@ function changeTheme()
 		'--text_input_color',
         '--table_border',
         '--table_border_center',
+		'--table_td_selected_background',
 		'--text_monster_name_water_color',
 		'--text_monster_name_fire_color',
 		'--text_monster_name_earth_color',
 		'--text_monster_name_light_color',
 		'--text_monster_name_dark_color',
+		'--div_selector_result_background',
+		'--div_selector_result_success_background'
     ];
     
     theme = (theme == 'normal') ? 'dark' : 'normal';
